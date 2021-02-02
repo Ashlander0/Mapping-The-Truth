2eb1ecdfdc9fb22da0339d0ad10219b2742f99ce
Matthew <ashlander@live.com>
2021-02-02 02:02:18 -0600

    Resized map

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,10 +8,6 @@
 		var cartoURL = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
 		var cartoAttrib = '&copy; ' + osmLink + ' &copy; ' + cartoLink;
 
-		//Stamen Toner tiles attribution and URL
-		var stamenURL = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}';
-		var stamenAttrib = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
-
 		//Elevation tiles attribution and URL
 		var ElevationLink = '<a href="https://www.openstreetmap.org/copyright">Elevation</a>';
 		var ElevationURL = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png';
@@ -21,24 +17,16 @@
 		var ElevationMap = L.tileLayer(ElevationURL, {attribution: ElevationAttrib});
 		var osmMap = L.tileLayer(osmURL, {attribution: osmAttrib});
 		var cartoMap = L.tileLayer(cartoURL, {attribution: cartoAttrib});
-		var stamenMap = L.tileLayer(stamenURL,{
-			attribution: stamenAttrib,
-			subdomains: 'abcd',
-			minZoom: 0,
-			maxZoom: 20,
-			ext: 'png'
-		});
 
 		//Map creation
 		var map = L.map('map',{
 			layers: [osmMap]
-		}).setView([40.25631414421629, -98.36258169550734], 5);
+		}).setView([40.25631414421629, -98.36258169550734], 4);
 
 		//Base layers definition and addition
 		var baseLayers = {
-			"OSM Mapnik": osmMap,
-			"Carto DarkMatter": cartoMap,
-			"Stamen Toner": stamenMap,
+			"Light Mode": osmMap,
+			"Dark Mode": cartoMap,
 			"Elevation": ElevationMap
 		};
 
