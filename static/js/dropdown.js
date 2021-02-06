1229f5e88cf98134d4da83d6f06b4d432f726b9a
Matthew <ashlander@live.com>
2021-02-06 12:41:17 -0600

    branch fixed

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -13,7 +13,7 @@ function addToBFDropdown(response) {
 
 function addToADropdown(response) {
 	for (i = 0; i < Object.keys(response).length; i++) {
-		var loc = `${response[i].city}, ${response[i].state}: ${response[i].date}`
+		var loc = `${response[i].city}, ${response[i].state}`
 		var value = Object.keys(response)[i];
 		alienDropdown.append('option').text(`${loc}`).property('value', value);
 	};
