8e4ddbe39fba4ba06cdff9353345e8f3735f1fad
Jasper Beachy <jbeach17@gmail.com>
2021-02-06 10:16:51 -0600

    granim added

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,24 @@
+var granimInstance = new Granim({
+    element: '#canvas-complex',
+    direction: 'left-right',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                [
+                    { color: '#833ab4', pos: .2 },
+                    { color: '#fd1d1d', pos: .8 },
+                    { color: '#38ef7d', pos: 1 }
+                ], [
+                    { color: '#40e0d0', pos: 0 },
+                    { color: '#ff8c00', pos: .2 },
+                    { color: '#ff0080', pos: .75 }
+                ],
+            ]
+        }
+    }
+});
+		
 		//OSM tiles attribution and URL
 		var osmLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 		var osmURL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
